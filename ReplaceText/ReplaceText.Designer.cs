0b8761561244e6f3001efe8e697c53026f22776a
165052.pham.minh <pvminh17@gmail.com>
2023-06-05 08:59:34 +0700

    Initial commit

---
+++ ReplaceText/ReplaceText.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ReplaceText
+{
+    partial class ReplaceText
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSourceTxt = new System.Windows.Forms.RichTextBox();
+            this.txtMapBinding = new System.Windows.Forms.RichTextBox();
+            this.txtResultText = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxSourceType = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnFindAllVietnameseText = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSourceTxt
+            // 
+            this.txtSourceTxt.Location = new System.Drawing.Point(12, 96);
+            this.txtSourceTxt.Name = "txtSourceTxt";
+            this.txtSourceTxt.Size = new System.Drawing.Size(871, 364);
+            this.txtSourceTxt.TabIndex = 0;
+            this.txtSourceTxt.Text = "";
+            // 
+            // txtMapBinding
+            // 
+            this.txtMapBinding.Location = new System.Drawing.Point(914, 96);
+            this.txtMapBinding.Name = "txtMapBinding";
+            this.txtMapBinding.Size = new System.Drawing.Size(607, 364);
+            this.txtMapBinding.TabIndex = 1;
+            this.txtMapBinding.Text = "";
+            // 
+            // txtResultText
+            // 
+            this.txtResultText.Location = new System.Drawing.Point(12, 493);
+            this.txtResultText.Name = "txtResultText";
+            this.txtResultText.Size = new System.Drawing.Size(1509, 397);
+            this.txtResultText.TabIndex = 2;
+            this.txtResultText.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Source Text:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(914, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Map binding: e.g. string=replacement";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 475);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Result Text:";
+            // 
+            // comboBoxSourceType
+            // 
+            this.comboBoxSourceType.FormattingEnabled = true;
+            this.comboBoxSourceType.Location = new System.Drawing.Point(241, 12);
+            this.comboBoxSourceType.Name = "comboBoxSourceType";
+            this.comboBoxSourceType.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxSourceType.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(125, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Source Text Type:";
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(494, 17);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 8;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // btnFindAllVietnameseText
+            // 
+            this.btnFindAllVietnameseText.Location = new System.Drawing.Point(665, 17);
+            this.btnFindAllVietnameseText.Name = "btnFindAllVietnameseText";
+            this.btnFindAllVietnameseText.Size = new System.Drawing.Size(218, 23);
+            this.btnFindAllVietnameseText.TabIndex = 9;
+            this.btnFindAllVietnameseText.Text = "Find all Vietnamese text";
+            this.btnFindAllVietnameseText.UseVisualStyleBackColor = true;
+            this.btnFindAllVietnameseText.Click += new System.EventHandler(this.btnFindAllVietnameseText_Click);
+            // 
+            // ReplaceText
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1533, 902);
+            this.Controls.Add(this.btnFindAllVietnameseText);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBoxSourceType);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResultText);
+            this.Controls.Add(this.txtMapBinding);
+            this.Controls.Add(this.txtSourceTxt);
+            this.Name = "ReplaceText";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RichTextBox txtSourceTxt;
+        private RichTextBox txtMapBinding;
+        private RichTextBox txtResultText;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ComboBox comboBoxSourceType;
+        private Label label4;
+        private Button btnExecute;
+        private Button btnFindAllVietnameseText;
+    }
+}
---
+++ ReplaceText/ReplaceText.cs
@@ -0,0 +1,224 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ReplaceText
+{
+    public partial class ReplaceText : Form
+    {
+        private const string PATTERN_HTML = @"[\p{L}\p{N}\s\!\?\.\,\(\)\/\\\-]*[ĂăÂâĐđÊêƠơƯưÀÁÃẠẰắẴẶỀếỄệÈÉẺẼÌÍỈĨÒÓÕỌỒốỖỘỜỚỠỢÙÚŨỤỪỨỮỰỲÝỶỸàáãạằẵặềếễệèéẻẽìíỉĩòóõọồốỗộờớỡợùúũụừứữựỳýỷỹ]+[\p{L}\p{N}\s\!\?\.\,\(\)\/\\\-]*";
+        private const string PATTERN_JS = @"[\""\']+[\p{L}\p{N}\s\!\?\.\,\(\)\:\/\\\-]*[ĂăÂâĐđÊêƠơƯưÀÁÃẠẰắẴẶỀếỄệÈÉẺẼÌÍỈĨÒÓÕỌỒốỖỘỜỚỠỢÙÚŨỤỪỨỮỰỲÝỶỸàáãạằẵặềếễệèéẻẽìíỉĩòóõọồốỗộờớỡợùúũụừứữựỳýỷỹ]+[\p{L}\p{N}\s\!\?\.\,\(\)\:\/\\-]*[\""\']+";
+        private const string PATTERN_CS = @"[\""\']+[\p{L}\p{N}\s\!\?\.\,\(\)\:\/\\\-]*[ĂăÂâĐđÊêƠơƯưÀÁÃẠẰắẴẶỀếỄệÈÉẺẼÌÍỈĨÒÓÕỌỒốỖỘỜỚỠỢÙÚŨỤỪỨỮỰỲÝỶỸàáãạằẵặềếễệèéẻẽìíỉĩòóõọồốỗộờớỡợùúũụừứữựỳýỷỹ]+[\p{L}\p{N}\s\!\?\.\,\(\)\:\/\\\-]*[\""\']+";
+        public ReplaceText()
+        {
+            InitializeComponent();
+            InitializeComponentCustom();
+        }
+
+        private void InitializeComponentCustom()
+        {
+            comboBoxSourceType.Items.Add("CSHTML");
+            comboBoxSourceType.Items.Add("JS");
+            comboBoxSourceType.Items.Add("C#");
+            comboBoxSourceType.SelectedIndex = 0;
+
+        }
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(comboBoxSourceType.Text)) return;
+
+            String rs = String.Empty;
+            switch (comboBoxSourceType.SelectedIndex)
+            {
+                case 0:
+                    rs = ReplaceVietnameseInCsHtml();
+                    break;
+                case 1:
+                    rs = ReplaceVietnameseInJS();
+                    break;
+                case 2:
+                    rs = ReplaceVietnameseInCS();
+                    break;
+            }
+            txtResultText.Text = rs;
+        }
+
+        private string ReplaceVietnameseInCS()
+        {
+            try
+            {
+                List<String> rs = new List<String>();
+                Dictionary<string, string> map = genarateMapBinding();
+                string[] linesMap = txtSourceTxt.Text.Split(new[] { '\n', '\r' });
+                foreach (string line in linesMap)
+                {
+                    string key = getContainKey(map, line);
+                    if (!String.IsNullOrEmpty(key))
+                    {
+                        rs.Add(line.Replace(key, map[key]) + " // " + key);
+                    }
+                    else
+                    {
+                        rs.Add(line);
+                    }
+                }
+                return String.Join("\n", rs.ToArray());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return String.Empty;
+            }
+        }
+
+        private string ReplaceVietnameseInJS()
+        {
+            try
+            {
+                List<String> rs = new List<String>();
+                Dictionary<string, string> map = genarateMapBinding();
+                string[] linesMap = txtSourceTxt.Text.Split(new[] { '\n', '\r' }) ;
+                foreach (string line in linesMap)
+                {
+                    string key = getContainKey(map, line);
+                    if (!String.IsNullOrEmpty(key))
+                    {
+                        rs.Add(line.Replace(key, map[key]) + " //" + key);
+                    }
+                    else
+                    {
+                        rs.Add(line);
+                    }
+                }
+                return String.Join("\n", rs.ToArray());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return String.Empty;
+            }
+        }
+
+        private string ReplaceVietnameseInCsHtml()
+        {
+            try
+            {
+                List<String> rs = new List<String>();
+                Dictionary<string, string> map = genarateMapBinding();
+                string[] linesMap = txtSourceTxt.Text.Split(new[] { '\n', '\r' });
+                foreach (string line in linesMap)
+                {
+                    string key = getContainKey(map, line);
+                    if (!String.IsNullOrEmpty(key))
+                    {
+                        rs.Add(line.Replace(key, "@T(\"" + map[key] + "\")") + " @*" + key + "*@");
+                    }
+                    else
+                    {
+                        rs.Add(line);
+                    }
+                }
+                return String.Join("\n", rs.ToArray());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return String.Empty;
+            }
+        }
+
+        private string getContainKey(Dictionary<string, string> map, string line)
+        {
+            string curKey = string.Empty;
+            foreach (string key in map.Keys)
+            {
+                if (line.Contains(key) && key.Length > curKey.Length) { curKey = key; }
+            }
+            return curKey;
+        }
+
+        private Dictionary<string, string> genarateMapBinding()
+        {
+            Dictionary<string, string> map = new Dictionary<string, string>();
+            string[] linesMap = txtMapBinding.Text.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string line in linesMap)
+            {
+                string[] arr = line.Split('=');
+                if (!map.ContainsKey(arr[0].Trim()))
+                {
+                    map.Add(arr[0].Trim(), arr[1].Trim());
+                }
+            }
+            return map;
+        }
+
+        private void btnFindAllVietnameseText_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String rs = String.Empty;
+                switch (comboBoxSourceType.SelectedIndex)
+                {
+                    case 0:
+                        rs = FindAllVietnameseInCsHtml(txtSourceTxt.Text);
+                        break;
+                    case 1:
+                        rs = FindAllVietnameseInJS(txtSourceTxt.Text);
+                        break;
+                    case 2:
+                        rs = FindAllVietnameseInCS(txtSourceTxt.Text);
+                        break;
+                }
+                txtResultText.Text = rs;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private String FindAllVietnameseInCS(String strSource)
+        {
+            // Remove single-line comments starting with "//"
+            strSource = Regex.Replace(strSource, @"//.*", "");
+
+            // Remove multi-line comments starting with "/*" and ending with "*/"
+            strSource = Regex.Replace(strSource, @"/\*.*?\*/", "", RegexOptions.Singleline);
+
+            return findStringWithPattern(strSource, PATTERN_CS);
+        }
+
+        private String FindAllVietnameseInJS(String strSource)
+        {
+            // Remove single-line comments starting with "//"
+            strSource = Regex.Replace(strSource, @"//.*", "");
+
+            // Remove multi-line comments starting with "/*" and ending with "*/"
+            strSource = Regex.Replace(strSource, @"/\*.*?\*/", "", RegexOptions.Singleline);
+            return findStringWithPattern(strSource, PATTERN_JS);
+        }
+
+        private String FindAllVietnameseInCsHtml(String strSource)
+        {
+            // Remove single-line comments starting with "@*"
+            strSource = Regex.Replace(strSource, @"@*\s*@.*?(?=\r?\n|$)", "", RegexOptions.Singleline);
+
+            // Remove multi-line comments starting with "@* ... *@"
+            strSource = Regex.Replace(strSource, @"@*\s*@.*?@\*", "", RegexOptions.Singleline);
+            return findStringWithPattern(strSource, PATTERN_HTML);
+        }
+
+        private String findStringWithPattern(String input, String pattern)
+        {
+            Regex regex = new Regex(pattern, RegexOptions.Multiline);
+            MatchCollection matches = regex.Matches(input);
+            StringBuilder sbResult = new StringBuilder();
+            foreach (Match match in matches)
+            {
+                String str = match.Value.Replace("\t", "").Trim().Trim('\t').Trim().Trim(',').Trim();
+                if (!String.IsNullOrEmpty(str))
+                    sbResult.AppendLine(str);
+            }
+            return sbResult.ToString();
+        }
+    }
+}
